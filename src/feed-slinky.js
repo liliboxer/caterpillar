cc854d03be3a221ea211ebcaeedf6b31985bb76c
Lili Boxer <ninaliboxer@gmail.com>
2019-04-26 10:43:21 -0700

    tdd almost working

--- src/feed-slinky.js
+++ src/feed-slinky.js
@@ -1,5 +1,9 @@
-function feedSlinky() {
+function feedSlinky(parentElement, donutName) {
+    const span = document.createElement('span');
+    span.classList.add('part');
+    span.classList.add(donutName);
 
+    parentElement.appendChild(span);
 }
 
 export default feedSlinky;
--- test/tests.js
+++ test/tests.js
@@ -2,16 +2,18 @@ import feedSlinky from '../src/feed-slinky.js';
 
 const test = QUnit.test;
 
-test('put donut into slinky', function(assert) {
+test('adds donut to slinky', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-    const donutColor = 'red';
-    const mockSlinkyBody = 
+    const donutName = 'red';
+    const parentElement = document.createElement('div');
+    const expected = "<span class='part red'></span>";
 
     //Act 
     // Call the function you're testing and set the result to a const
-    feedSlinky()
+    feedSlinky(parentElement, donutName);
 
     //Assert
-    assert.equal(true, false);
+    const parentInnerHTML = parentElement.innterHTML;
+    assert.equal(parentInnerHTML, expected);
 });
