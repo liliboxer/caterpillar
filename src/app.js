1e675fe680337102ffa8bd49aecc0060420ca6b1
Lili Boxer <ninaliboxer@gmail.com>
2019-04-26 10:17:41 -0700

    test set up

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const donutButtons = document.querySelectorAll('.donut');
+
+for (let i = 0; i < donutButtons.length; i++) {
+    const donutButton = donutButtons[i];
+    console.log(donutButton);
+    
+}
+
+// donutButton.addEventListener('click', () => {
+    
+// });
--- test/tests.js
+++ test/tests.js
@@ -1,11 +1,16 @@
+import feedSlinky from '../src/feed-slinky.js';
+
 const test = QUnit.test;
 
-test('time to test a function', function(assert) {
+test('put donut into slinky', function(assert) {
     //Arrange
     // Set up your parameters and expectations
+    const donutColor = 'red';
+    const mockSlinkyBody = 
 
     //Act 
     // Call the function you're testing and set the result to a const
+    feedSlinky()
 
     //Assert
     assert.equal(true, false);
