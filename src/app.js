1d1bc20c5de91edd5be1b5c81056f0308bf91621
Lili Boxer <ninaliboxer@gmail.com>
2019-05-01 21:06:21 -0700

    const tai in app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import feedSlinky from '../src/feed-slinky.js';
 
 const donutButtons = document.querySelectorAll('.donut');
 const body = document.getElementById('body');
+const tail = document.getElementById('tail');
 
 for(let i = 0; i < donutButtons.length; i++) {
     const donutButton = donutButtons[i];
