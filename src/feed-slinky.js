04f8ffb56381b71a9c086d5aa10a0de8b3524619
Lili Boxer <ninaliboxer@gmail.com>
2019-05-02 10:08:55 -0700

    working?

--- src/feed-slinky.js
+++ src/feed-slinky.js
@@ -1,7 +1,6 @@
 function feedSlinky(parentElement, donutName, tail) {
     const span = document.createElement('span');
     span.classList.add('part');
-    span.classList.add('dance');
     span.classList.add(donutName);
     
     parentElement.insertBefore(span, tail);
